71994d1a1cf39e86ad5a2fa9ec28d6a4dc0a9aaf
nandowastaken <fernandojorge.cavalcantegomes@gmail.com>
2024-06-22 22:58:13 -0300

    feat: roll dices

--- src/index.js
+++ src/index.js
@@ -10,6 +10,8 @@ const client = new Client({
   ],
 });
 
+const regex = /\b\d+d\d+\b/;
+
 client.on("ready", (c) => {
   console.log(`✅ ${c.user.tag} is online.`);
 });
@@ -21,6 +23,25 @@ client.on("messageCreate", (message) => {
 
   if (message.content === "Aurora") {
     message.reply("Eu!");
+  }
+
+  if (regex.test(message.content)) {
+    const text = message.content;
+
+    const info = text.split("d");
+    const numOfRolls = parseInt(info[0]);
+    const diceType = parseInt(info[1]);
+
+    const diceResults = [];
+    var totalSum = 0;
+
+    for (var i = 0; i < numOfRolls; i++) {
+      totalSum += Math.floor(Math.random() * diceType) + 1;
+    }
+
+    reply = String(totalSum);
+
+    message.reply(reply);
   }
 });
 
